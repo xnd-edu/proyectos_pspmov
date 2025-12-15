6217456653d6a885bddf90026ba90db80c5a13be
xnd-edu <alexander.caballero@educa.madrid.org>
2025-12-15 02:20:24 +0100

    Fix

--- Navigation/app/src/main/java/com/example/navigation/domain/usecases/jsonplaceholder/DeletePostUseCase.kt
+++ Navigation/app/src/main/java/com/example/navigation/domain/usecases/jsonplaceholder/DeletePostUseCase.kt
@@ -1,11 +1,11 @@
-package com.example.navigation.domain.usecases.conductores
+package com.example.navigation.domain.usecases.jsonplaceholder
 
 import com.example.navigation.common.NetworkResult
 import com.example.navigation.data.JsonPlaceholderRepository
 import com.example.navigation.domain.model.JsonPlaceholderPost
 import javax.inject.Inject
 
-class DeleteConductorUseCase @Inject constructor(
+class DeletePostUseCase @Inject constructor(
     private val jsonPlaceholderRepository: JsonPlaceholderRepository
 ) {
     suspend operator fun invoke(jsonPlaceholderPost: JsonPlaceholderPost): NetworkResult<Unit> = jsonPlaceholderRepository.deletePost(jsonPlaceholderPost.id)
--- Navigation/app/src/main/java/com/example/navigation/domain/usecases/jsonplaceholder/GetPostByIdUseCase.kt
+++ Navigation/app/src/main/java/com/example/navigation/domain/usecases/jsonplaceholder/GetPostByIdUseCase.kt
@@ -1,11 +1,11 @@
-package com.example.navigation.domain.usecases.conductores
+package com.example.navigation.domain.usecases.jsonplaceholder
 
 import com.example.navigation.common.NetworkResult
 import com.example.navigation.data.JsonPlaceholderRepository
 import com.example.navigation.domain.model.JsonPlaceholderPost
 import javax.inject.Inject
 
-class GetConductorByDniUseCase @Inject constructor(
+class GetPostByIdUseCase @Inject constructor(
     private val jsonPlaceholderRepository: JsonPlaceholderRepository
 ) {
     suspend operator fun invoke(id: Int): NetworkResult<JsonPlaceholderPost> = jsonPlaceholderRepository.getPost(id)
